8fd198f38a9b2eae53f86c451cb1d2f382131b3f
Evgenii Vlasov <evjvlasov@gmail.com>
2019-10-05 01:03:28 +0300

    SpringTest2

---
+++ src/main/java/springtest/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package springtest;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Requiem";
+    }
+}
---
+++ src/main/java/springtest/Music.java
@@ -0,0 +1,5 @@
+package springtest;
+
+public interface Music {
+    String getSong();
+}
